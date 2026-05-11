15d72fd2bdf6712f3ae1b9b474fe49d275548d44
Ubuntu <mryummasood@yahoo.com>
2026-05-11 20:22:18 +0000

    fix all issues: docker, linting, selenium, dockerfile

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -1,20 +1,26 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-import pytest
+from selenium.webdriver.chrome.service import Service
+
 
 def get_driver():
     opts = Options()
     opts.add_argument("--headless")
     opts.add_argument("--no-sandbox")
     opts.add_argument("--disable-dev-shm-usage")
-    return webdriver.Chrome(options=opts)
+    opts.add_argument("--disable-gpu")
+    opts.binary_location = "/usr/bin/chromium"
+    service = Service("/usr/bin/chromedriver")
+    return webdriver.Chrome(service=service, options=opts)
+
 
 def test_homepage_loads():
     driver = get_driver()
     driver.get("http://localhost:5000")
     assert "Welcome" in driver.page_source
     driver.quit()
+
 
 def test_form_submission():
     driver = get_driver()
